c8f05089036d0c4860127559be94d61d43b4e968
Kolbak <kushaevmarat@mail.ru>
2021-07-23 08:00:55 +0300

    maket with chakra

--- src/pages/user_account.js
+++ src/pages/user_account.js
@@ -3,7 +3,7 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 const UserAccount = () => (
   <Layout>
