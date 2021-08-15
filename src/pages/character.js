b8afdc3fc5ab457d2f222eadc301036e070eab5c
Florn67 <edgewalker909@gmail.com>
2021-08-15 11:19:26 +0300

    refactor: character

--- src/pages/character.js
+++ src/pages/character.js
@@ -19,39 +19,146 @@ const Character = () => {
       <div className="character-container">
           <div className="character-text">
               <h2 ref={changeName} className="character-header">.</h2>
-              <a href="#" className="OpenSea-link">View on OpenSea</a>
-              <ul className="character-details"><p className="character-details-header">Details<span className="character-detail-points"></span></p>
-                  <li className="character-detail">Birthday<span className="character-detail-points"></span><span className="character-detail-value">10.10.1010</span></li>
-                  <li className="character-detail">Traits<span className="character-detail-points"></span><span className="character-detail-value">12</span></li>
-                  <li className="character-detail">Background<span className="character-detail-points"></span><span className="character-detail-value">Purple</span>
-                      <p className="character-detail-occurs">Occurs in <span className="character-detail-occurs-value">10%</span></p>
+              <a href="#" className="OpenSea-link">
+                  View on OpenSea
+              </a>
+              <ul className="character-details"><p className="character-details-header">
+                  Details
+                  </p>
+
+                  <li className="character-detail">
+                      Birthday
+                      <span className="character-detail-points"></span>
+                      <span className="character-detail-value">
+                          10.10.1010
+                      </span>
                   </li>
-                  <li className="character-detail">Size<span className="character-detail-points"></span><span className="character-detail-value">Purple</span>
-                      <p className="character-detail-occurs">Occurs in <span className="character-detail-occurs-value">10%</span></p>
+
+                  <li className="character-detail">
+                      Traits
+                      <span className="character-detail-points"></span>
+                      <span className="character-detail-value">
+                          12
+                      </span>
                   </li>
-                  <li className="character-detail">Skin color<span className="character-detail-points"></span><span className="character-detail-value">2.5 inch</span>
-                      <p className="character-detail-occurs">Occurs in <span className="character-detail-occurs-value">10%</span></p>
+
+                  <li className="character-detail">
+                      Background
+                      <span className="character-detail-points"></span>
+                      <span className="character-detail-value">
+                          Purple
+                      </span>
+                      <p className="character-detail-occurs">
+                          Occurs in 
+                           <span className="character-detail-occurs-value"> 10%</span>
+                     </p>
                   </li>
-                  <li className="character-detail">Skin type<span className="character-detail-points"></span><span className="character-detail-value">Reptile (Wild-Animal)</span>
-                      <p className="character-detail-occurs">Occurs in <span className="character-detail-occurs-value">10%</span></p>
+
+                  <li className="character-detail">
+                      Size
+                      <span className="character-detail-points"></span>
+                      <span className="character-detail-value">
+                          Purple
+                      </span>
+                      <p className="character-detail-occurs">
+                          Occurs in 
+                           <span className="character-detail-occurs-value"> 10%</span>
+                      </p>
                   </li>
-                  <li className="character-detail">Head<span className="character-detail-points"></span><span className="character-detail-value">FAPP Cap (Basic-Common)</span>
-                      <p className="character-detail-occurs">Occurs in <span className="character-detail-occurs-value">10%</span></p>
+                  
+                  <li className="character-detail">
+                      Skin color
+                      <span className="character-detail-points"></span>
+                      <span className="character-detail-value">
+                          2.5 inch
+                      </span>
+                      <p className="character-detail-occurs">
+                          Occurs in 
+                           <span className="character-detail-occurs-value"> 10%</span>
+                      </p>
                   </li>
-                  <li className="character-detail">Eyes<span className="character-detail-points"></span><span className="character-detail-value">Cyber (Wild-Cyber)</span>
-                      <p className="character-detail-occurs">Occurs in <span className="character-detail-occurs-value">10%</span></p>
+
+                  <li className="character-detail">
+                      Skin type
+                      <span className="character-detail-points"></span>
+                      <span className="character-detail-value">
+                          Reptile (Wild-Animal)
+                      </span>
+                      <p className="character-detail-occurs">
+                          Occurs in 
+                           <span className="character-detail-occurs-value"> 10%</span>
+                      </p>
                   </li>
-                  <li className="character-detail">Mounth<span className="character-detail-points"></span><span className="character-detail-value">Joint (Basic-Rasta)</span>
-                      <p className="character-detail-occurs">Occurs in <span className="character-detail-occurs-value">10%</span></p>
+
+                  <li className="character-detail">
+                      Head
+                      <span className="character-detail-points"></span>
+                      <span className="character-detail-value">
+                          FAPP Cap (Basic-Common)
+                      </span>
+                      <p className="character-detail-occurs">
+                          Occurs in 
+                          <span className="character-detail-occurs-value"> 10%</span>
+                      </p>
                   </li>
-                  <li className="character-detail">Eyewear<span className="character-detail-points"></span><span className="character-detail-value">Cocktier Diamond (Dope-Precious)</span>
-                      <p className="character-detail-occurs">Occurs in <span className="character-detail-occurs-value">10%</span></p>
+
+                  <li className="character-detail">
+                      Eyes
+                      <span className="character-detail-points"></span>
+                      <span className="character-detail-value">
+                          Cyber (Wild-Cyber)
+                      </span>
+                      <p className="character-detail-occurs">
+                          Occurs in
+                          <span className="character-detail-occurs-value"> 10%</span>
+                      </p>
                   </li>
-                  <li className="character-detail">Necklace<span className="character-detail-points"></span><span className="character-detail-value">Cuban Diamond Chain (Dope-Precious)</span>
-                      <p className="character-detail-occurs">Occurs in <span className="character-detail-occurs-value">10%</span></p>
+
+                  <li className="character-detail">
+                      Mounth
+                      <span className="character-detail-points"></span>
+                      <span className="character-detail-value">
+                          Joint (Basic-Rasta)
+                      </span>
+                      <p className="character-detail-occurs">
+                          Occurs in 
+                          <span className="character-detail-occurs-value"> 10%</span>
+                      </p>
                   </li>
-                  <li className="character-detail">Back item<span className="character-detail-points"></span><span className="character-detail-value">Angel Wings (Wild-Afterlife)</span>
-                      <p className="character-detail-occurs">Occurs in <span className="character-detail-occurs-value">10%</span></p>
+
+                  <li className="character-detail">
+                      Eyewear
+                      <span className="character-detail-points"></span>
+                      <span className="character-detail-value">
+                          Cocktier Diamond (Dope-Precious)
+                      </span>
+                      <p className="character-detail-occurs">
+                          Occurs in 
+                          <span className="character-detail-occurs-value"> 10%</span>
+                      </p>
+                  </li>
+
+                  <li className="character-detail">
+                      Necklace
+                      <span className="character-detail-points"></span>
+                      <span className="character-detail-value">
+                          Cuban Diamond Chain (Dope-Precious)
+                      </span>
+                      <p className="character-detail-occurs">
+                          Occurs in 
+                          <span className="character-detail-occurs-value"> 10%</span>
+                      </p>
+                  </li>
+                  <li className="character-detail">
+                      Back item
+                      <span className="character-detail-points"></span>
+                      <span className="character-detail-value">
+                          Angel Wings (Wild-Afterlife)
+                          </span>
+                      <p className="character-detail-occurs">
+                          Occurs in
+                          <span className="character-detail-occurs-value"> 10%</span>
+                      </p>
                   </li>
               </ul>
           </div>
