eda62080198f0672ad9f272409a8c617e3b89e2b
Kolbak <kushaevmarat@mail.ru>
2021-08-15 20:28:23 +0300

    fixes

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -26,49 +26,8 @@ import anakin from '../images/anakin.jpg'
 import wizard from '../images/wizard.jpg'
 
 const Gallery = () => {
-  function animate(_e) {
-    let mouseX = 0;
-    let mouseY = 0;
-
-    let target, label;
-    target = _e.target;
-
-    let coords = target.getBoundingClientRect();
-    mouseX = _e.clientX - coords.left - coords.width / 2;
-    mouseY = _e.clientY - coords.top - coords.height / 2;
-
-    let mousePX = mouseX / coords.width;
-    let mousePY = mouseY / coords.height;
-
-    let rX = mousePX * 15;
-    let rY = mousePY * -15;
-
-    let tX = mousePX * 20;
-    let tY = mousePY * 20;
-
-    label = document.querySelector(`#label-${_e.target.getAttribute('id')}`);
-    label.style.transform = `translate(${tX}px, ${tY}px) rotateY(${rX}deg) rotateX(${rY}deg)`;
-    target.style.transform = `translate(${tX}px, ${tY}px) rotateY(${rX}deg) rotateX(${rY}deg)`;
-
-    target.onmouseleave = () => {
-      label.style.transition = '.6s';
-      target.style.transition = '.6s';
-
-      setTimeout(() => {
-        label.style.transform = `translate(${0}px, ${0}px) rotateY(${0}deg) rotateX(${0}deg)`;
-        target.style.transform = `translate(${0}px, ${0}px) rotateY(${0}deg) rotateX(${0}deg)`;
-
-        setTimeout(() => {
-          label.style.transition = '0s';
-          target.style.transition = '0s';
-        }, 100)
-      }, 300)
-    }
-  }
-
   //? dummy data
   let arrData = [];
-  // function addData() {
   for (let i = 0; i < 6; i++) {
     arrData.push({
       name: 'Character №' + i,
@@ -76,34 +35,7 @@ const Gallery = () => {
       src: [anakin, wizard][Math.floor(Math.random() * 2)],
     })
   }
-  //   console.log('arrData :>> ', arrData.length);
-  // }
-  // addData();
   const [data, setData] = useState(arrData);
-
-  // let grid = [];
-  // function createGrid(_data) {
-  // for (let i = 0, l = data.length; i < l; i++) {
-  //   grid.push(
-  //     <div className="char-card" key={i}>
-  //       <div className="char-body">
-  //         <figure className="front">
-  //           <a className="char-link">
-  //             <span id={'label-char-' + data[i].id}>{data[i].name}</span>
-  //             <img id={'char-' + data[i].id} onMouseMove={animate} src={data[i].src} alt={'Character number' + data[i]} />
-  //           </a>
-  //         </figure>
-  //       </div>
-  //     </div>
-  //   )
-  // }
-  // }
-  // createGrid(data);
-
-  // function showMoreData() {
-  //   addData();
-  //   setData(arrData);
-  // }
 
   //? аккордион
   const accordionItemStyle = {
@@ -122,16 +54,11 @@ const Gallery = () => {
     outline: 'none',
     boxShadow: 'none',
   }
-  // const BoxStyle = {
-  //   color: '#ababab',
-  //   // fontWeight: 'bold',
-  // }
 
   //? кнопка ShowMore
   function ButtonCustomShowMore({ children, className }) {
     return <Box
       className={className}
-      // onClick={showMoreData}
       as="button"
       border="none"
       color="#0a0e16"
