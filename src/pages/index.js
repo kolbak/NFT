39a964d22dd2bd7f998eca5b1ba1bda66d51ecf1
Kolbak <kushaevmarat@mail.ru>
2021-08-28 09:17:11 +0300

    add animations || + LIBRAry

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import { Link } from "gatsby"
+import Fade from 'react-reveal/Fade';
 import {
   Box,
   Accordion,
@@ -35,6 +36,8 @@ import discord from "../images/icons8-discord.svg"
 import ig from "../images/icons8-instagram-old.svg"
 import twitter from "../images/icons8-twitter.svg"
 import opensea from "../images/opensea.svg"
+
+import arrow from "../images/icons8-down-arrow-50.png"
 
 const IndexPage = () => {
   const isBrowser = typeof window !== "undefined"
@@ -138,6 +141,17 @@ const IndexPage = () => {
     )
   }
 
+  //? анимация
+  // function animation() {
+
+  //   function offset(el) {
+  //     const rect = el.getBoundingClientRect(),
+  //       scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  //       scrollTop = window.pageyOffset || document.documentElement.scrollTop;
+  //     return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  //   }
+  // };
+
   return (
     <>
       <Layout>
@@ -172,165 +186,213 @@ const IndexPage = () => {
           </div>
           <div className="gen-wrap">
             <div id="content-block-storyline" className="content-block storyline">
-              <div className="text-wrap">
-                <div className="text">
-                  <h2>STORYLINE</h2>
-                  <p>
-                    We are a team of artists, developers and financial analysts
-                    that discovered Family Phallus Planet - FAPP. Family Phallus
-                    Planet - FAPP is an Ethereum Blockchain planet with 10,000
-                    unique 2.5 inch NFT Phalluses.
-                  </p>
-                  <br className="display-none" />
-                  <br />
-                  <p>
-                    FAPP is not boring, every Phallus here knows how to deal with
-                    boredom! Look at Phalluses! They know that they're small, but
-                    they DGAF about it. They support each other and think about
-                    their strengths while humans fixated on their "problems".
-                  </p>
-                  <p>
-                    Mint your phallus and join the community where everyone can
-                    find friends and forget about stupid stereotypes!
-                  </p>
+              <Fade left>
+                <div className="text-wrap">
+                  <div className="text">
+                    <h2>STORYLINE</h2>
+                    <p>
+                      We are a team of artists, developers and financial analysts
+                      that discovered Family Phallus Planet - FAPP. Family Phallus
+                      Planet - FAPP is an Ethereum Blockchain planet with 10,000
+                      unique 2.5 inch NFT Phalluses.
+                    </p>
+                    <br className="display-none" />
+                    <br />
+                    <p>
+                      FAPP is not boring, every Phallus here knows how to deal with
+                      boredom! Look at Phalluses! They know that they're small, but
+                      they DGAF about it. They support each other and think about
+                      their strengths while humans fixated on their "problems".
+                    </p>
+                    <p>
+                      Mint your phallus and join the community where everyone can
+                      find friends and forget about stupid stereotypes!
+                    </p>
+                  </div>
+                  <ButtonCustomMint className="toMint">
+                    <Link to="/mint">
+                      <span>Mint</span>
+                    </Link>
+                  </ButtonCustomMint>
                 </div>
-                <ButtonCustomMint className="toMint">
-                  <Link to="/mint">
-                    <span>Mint</span>
-                  </Link>
-                </ButtonCustomMint>
-              </div>
-              <img src={storyline} alt="" />
+              </Fade>
+              <Fade right>
+                <img src={storyline} alt="" />
+              </Fade>
             </div>
             <div id="content-block-nft" className="content-block nft">
-              <div className="info">
-                <h2>NFT</h2>
-                <div className="about">
-                  <h3>10,000 UNIQUE NFT</h3>
-                  <h3>98 UNIQUE PROPERTIES</h3>
-                  <h3>7 STYLES</h3>
-                  <h3>3 RARITY LEVELS</h3>
+              <Fade right>
+                <div className="info">
+                  <h2>NFT</h2>
+                  <div className="about">
+                    <h3>10,000 UNIQUE NFT</h3>
+                    <h3>98 UNIQUE PROPERTIES</h3>
+                    <h3>7 STYLES</h3>
+                    <h3>3 RARITY LEVELS</h3>
+                  </div>
+                  <p>
+                    Every Phallus has unique combination of background color, skin
+                    color, skin type, eyes, mouth, head, eyewear, necklace and back
+                    item
+                  </p>
                 </div>
-                <p>
-                  Every Phallus has unique combination of background color, skin
-                  color, skin type, eyes, mouth, head, eyewear, necklace and back
-                  item
-                </p>
-              </div>
-              <div className="levels">
-                <div className="dope level">
-                  <h3>
-                    <span>15%</span> DOPE LEVEL
-                  </h3>
-                  <div className="icons">
-                    <figure>
-                      <img src={rasta} alt="" />
-                      <figcaption>Rasta style</figcaption>
-                    </figure>
+              </Fade>
+              <Fade left cascade>
+                <div className="levels">
+                  <div className="dope level">
+                    <h3>
+                      <span>15%</span> DOPE LEVEL
+                    </h3>
+                    <div className="icons">
+                      <figure>
+                        <img src={rasta} alt="" />
+                        <figcaption>Rasta style</figcaption>
+                      </figure>
+                    </div>
+                  </div>
+                  <div className="wild level">
+                    <h3>
+                      <span>35%</span> WILD LEVEL
+                    </h3>
+                    <div className="icons">
+                      <figure>
+                        <img src={superhero} alt="" />
+                        <figcaption>Superhero style</figcaption>
+                      </figure>
+                      <figure>
+                        <img src={cyber} alt="" />
+                        <figcaption>Cyber style</figcaption>
+                      </figure>
+                      <figure>
+                        <img src={afterlife} alt="" />
+                        <figcaption>Afterlife style</figcaption>
+                      </figure>
+                    </div>
+                  </div>
+                  <div className="basic level">
+                    <h3>
+                      <span>50%</span> BASIC LEVEL
+                    </h3>
+                    <div className="icons">
+                      <figure>
+                        <img src={basic} alt="" />
+                        <figcaption>Basic style</figcaption>
+                      </figure>
+                      <figure>
+                        <img src={wild} alt="" />
+                        <figcaption>Wild style</figcaption>
+                      </figure>
+                      <figure>
+                        <img src={protection} alt="" />
+                        <figcaption>Protection style</figcaption>
+                      </figure>
+                      <figure>
+                        <img src={precious} alt="" />
+                        <figcaption>Precious style</figcaption>
+                      </figure>
+                    </div>
                   </div>
                 </div>
-                <div className="wild level">
-                  <h3>
-                    <span>35%</span> WILD LEVEL
-                  </h3>
-                  <div className="icons">
-                    <figure>
-                      <img src={superhero} alt="" />
-                      <figcaption>Superhero style</figcaption>
-                    </figure>
-                    <figure>
-                      <img src={cyber} alt="" />
-                      <figcaption>Cyber style</figcaption>
-                    </figure>
-                    <figure>
-                      <img src={afterlife} alt="" />
-                      <figcaption>Afterlife style</figcaption>
-                    </figure>
-                  </div>
-                </div>
-                <div className="basic level">
-                  <h3>
-                    <span>50%</span> BASIC LEVEL
-                  </h3>
-                  <div className="icons">
-                    <figure>
-                      <img src={basic} alt="" />
-                      <figcaption>Basic style</figcaption>
-                    </figure>
-                    <figure>
-                      <img src={wild} alt="" />
-                      <figcaption>Wild style</figcaption>
-                    </figure>
-                    <figure>
-                      <img src={protection} alt="" />
-                      <figcaption>Protection style</figcaption>
-                    </figure>
-                    <figure>
-                      <img src={precious} alt="" />
-                      <figcaption>Precious style</figcaption>
-                    </figure>
-                  </div>
-                </div>
-              </div>
+              </Fade>
             </div>
             <div id="content-block-roadmap" className="content-block roadmap">
-              <h2>ROADMAP</h2>
+              <Fade left>
+                <h2>ROADMAP</h2>
+              </Fade>
               {screenWidth > 750 &&
                 <div className="points">
-                  <div className="point p5">
-                    <h3><span>5%</span> PHALLUSES NEED FRIENDS!</h3>
-                    <img src={whiteIMG} alt="" />
-                    <p>
-                      Giveaway! 100 free Phalluses to people who reposted our first
-                      post on twitter!
-                    </p>
-                  </div>
-                  <div className="point p15 right">
-                    <h3><span>15%</span> FAPP IS EVERYWHERE!</h3>
-                    <img src={whiteIMG} alt="" />
-                    <p>
-                      We will invest up to 80% of the funds to promote the project
-                      to make our Phalluses more expensive and desired!
-                    </p>
-                    <p>
-                      We will need your help too! The most active Phalluses can join
-                      our team!
-                    </p>
-                  </div>
-                  <div className="point p30">
-                    <h3><span>30%</span> LOOK AT THE SKY!</h3>
-                    <img src={whiteIMG} alt="" />
-                    <p>
-                      5 Phalluses will be minted and air dropped to 5 random
-                      collectors!
-                    </p>
-                  </div>
-                  <div className="point p50 right">
-                    <h3><span>50%</span> SHOPPING SPREE!</h3>
-                    <img src={whiteIMG} alt="" />
-                    <p>
-                      Our official FAPP nft merch store will be unlocked for
-                      Phalluses.
-                    </p>
-                  </div>
-                  <div className="point p70">
-                    <h3><span>70%</span> ICED OUT WHO? ICED OUT WATCH!</h3>
-                    <img src={whiteIMG} alt="" />
-                    <p>
-                      We will give 1 Rolex date just 41 iced out watch piece to 1
-                      random collector with rare Phalluses
-                    </p>
-                  </div>
-                  <div className="point p80 right">
-                    <h3><span>80%</span> WHO WANT THE SMOKE?</h3>
-                    <img src={whiteIMG} alt="" />
-                    <p>FAPP gives 10 ETH to 10 random Phalluses.</p>
-                  </div>
-                  <div className="point p100">
-                    <h3><span>100%</span> IT IS CHARITY TIME</h3>
-                    <img src={whiteIMG} alt="" />
-                    <p>FAPP will make a 35 ETH donation to the AIDS Foundation!</p>
-                  </div>
+                  <Fade left>
+                    <div className="point p5">
+                      <h3><span>5%</span> PHALLUSES NEED FRIENDS!</h3>
+                      <img src={whiteIMG} alt="" />
+                      <p>
+                        Giveaway! 100 free Phalluses to people who reposted our first
+                        post on twitter!
+                      </p>
+                      <div className="arrow-wrap">
+                        <p>15%</p>
+                        <img className="arrow" src={arrow} alt="" />
+                      </div>
+                    </div>
+                  </Fade>
+                  <Fade right>
+                    <div className="point p15 right">
+                      <h3><span>15%</span> FAPP IS EVERYWHERE!</h3>
+                      <img src={whiteIMG} alt="" />
+                      <p>
+                        We will invest up to 80% of the funds to promote the project
+                        to make our Phalluses more expensive and desired!
+                      </p>
+                      <p>
+                        We will need your help too! The most active Phalluses can join
+                        our team!
+                      </p>
+                      <div className="arrow-wrap left">
+                        <p>30%</p>
+                        <img className="arrow" src={arrow} alt="" />
+                      </div>
+                    </div>
+                  </Fade>
+                  <Fade left>
+                    <div className="point p30">
+                      <h3><span>30%</span> LOOK AT THE SKY!</h3>
+                      <img src={whiteIMG} alt="" />
+                      <p>
+                        5 Phalluses will be minted and air dropped to 5 random
+                        collectors!
+                      </p>
+                      <div className="arrow-wrap">
+                        <p>50%</p>
+                        <img className="arrow" src={arrow} alt="" />
+                      </div>
+                    </div>
+                  </Fade>
+                  <Fade right>
+                    <div className="point p50 right">
+                      <h3><span>50%</span> SHOPPING SPREE!</h3>
+                      <img src={whiteIMG} alt="" />
+                      <p>
+                        Our official FAPP nft merch store will be unlocked for
+                        Phalluses.
+                      </p>
+                      <div className="arrow-wrap left">
+                        <p>70%</p>
+                        <img className="arrow" src={arrow} alt="" />
+                      </div>
+                    </div>
+                  </Fade>
+                  <Fade left>
+                    <div className="point p70">
+                      <h3><span>70%</span> ICED OUT WHO? ICED OUT WATCH!</h3>
+                      <img src={whiteIMG} alt="" />
+                      <p>
+                        We will give 1 Rolex date just 41 iced out watch piece to 1
+                        random collector with rare Phalluses
+                      </p>
+                      <div className="arrow-wrap">
+                        <p>80%</p>
+                        <img className="arrow" src={arrow} alt="" />
+                      </div>
+                    </div>
+                  </Fade>
+                  <Fade right>
+                    <div className="point p80 right">
+                      <h3><span>80%</span> WHO WANT THE SMOKE?</h3>
+                      <img src={whiteIMG} alt="" />
+                      <p>FAPP gives 10 ETH to 10 random Phalluses.</p>
+                      <div className="arrow-wrap left">
+                        <p>100%</p>
+                        <img className="arrow" src={arrow} alt="" />
+                      </div>
+                    </div>
+                  </Fade>
+                  <Fade left>
+                    <div className="point p100">
+                      <h3><span>100%</span> IT IS CHARITY TIME</h3>
+                      <img src={whiteIMG} alt="" />
+                      <p>FAPP will make a 35 ETH donation to the AIDS Foundation!</p>
+                    </div>
+                  </Fade>
                 </div>
               }
               {screenWidth <= 750 &&
