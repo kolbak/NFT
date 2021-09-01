11ef088272d473d739ed4ed905992240597a0450
Kolbak <kushaevmarat@mail.ru>
2021-09-01 19:35:28 +0300

    lit clean

--- src/pages/index.js
+++ src/pages/index.js
@@ -524,7 +524,7 @@ const IndexPage = () => {
                     </ul>
                   </AccordionItemCustom>
                   <AccordionItemCustom title="HOW CAN I GET A PHALLUS?">
-                    Click here <a href="https://nft-cockiz.com/mint">here</a>, connect your MetaMask wallet and mint (buy) your NFT Phallus.<br/>
+                    Click <a href="https://nft-cockiz.com/mint">here</a>, connect your MetaMask wallet and mint (buy) your NFT Phallus.<br/>
                     1 Phallus costs 0.08 ETH + GAS.
                   </AccordionItemCustom>
                   <AccordionItemCustom title="CAN I SEE WHAT PHALLUS I WILL GET?">
