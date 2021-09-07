1a093b52e00cf72c3d4e72328a2d380bee6c87c2
Kolbak <kushaevmarat@mail.ru>
2021-09-08 02:15:02 +0300

    add grid in user

--- src/pages/user_account.js
+++ src/pages/user_account.js
@@ -1,14 +1,29 @@
-import * as React from "react"
+import React, { useState } from "react"
 import {
   Box,
 } from "@chakra-ui/react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-
+import GalleryGrid from "../components/gallery_grid"
+import characterEx from "../images/character-ex.png"
 import './user_account.scss'
 
 const UserAccount = () => {
+  //? data bought
+  let arrData = [];
+  for (let i = 0; i < 3; i++) {
+    arrData.push({
+      name: `Character ${i + 1}`,
+      id: i + 1,
+      src: characterEx,
+      filters: {
+        bday: new Intl.DateTimeFormat('en-US').format(new Date()),
+      }
+    })
+  }
+  const [data, setData] = useState(arrData);
+
   function ButtonCustomConnect({ children, className }) {
     return (
       <Box
@@ -25,9 +40,7 @@ const UserAccount = () => {
       <div className="gen-wrap user-acc">
         <h1>#FAPP</h1>
         <div className="bought">
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
+          <GalleryGrid data={data} />
         </div>
         <ButtonCustomConnect className="user-account cnct-wallet">
           Connect your wallet
