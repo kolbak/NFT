60fc0cb23ee4b59a6370c707f45b024a1bfdb9c6
Kolbak <kushaevmarat@mail.ru>
2021-09-15 21:39:33 +0300

    char

--- src/pages/character.js
+++ src/pages/character.js
@@ -12,32 +12,37 @@ const Character = ({ location }) => {
   let name, id, filters;
   let src;
   if (isBrowser) {
-    console.log('location :>> ', new URL(location.href).searchParams.get('id'));
-    if(new URL(location.href).searchParams.get('id')) {
+    if (new URL(location.href).searchParams.get('id')) {
       //? запрос
       // .......
-      
+      src = char;
+      name = `Character ${new URL(location.href).searchParams.get('id')}`;
+      id = new URL(location.href).searchParams.get('id');
+      filters = {
+        bday: new Intl.DateTimeFormat("en-US").format(new Date()),
+      };
       // .......      
 
-    } else {
+    } 
+    // else {
       src = location.state ? location.state.src :
         (window.localStorage.getItem("details") && JSON.parse(window.localStorage.getItem("details")).src);
-        
+
       if (location.state) {
         name = location.state.name;
         id = location.state.id;
         filters = location.state.filters;
-  
+
         window.localStorage.setItem("details", JSON.stringify({ name, id, src, filters }));
       }
       else if (window.localStorage.getItem("details")) {
         let detObj = JSON.parse(window.localStorage.getItem("details"));
-  
+
         name = detObj.name;
         id = detObj.id;
         filters = detObj.filters;
       }
-    }
+    // }
   }
 
   return (
