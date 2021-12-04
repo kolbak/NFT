c0f5993166fd2abe0cde9df8607eaea344a0fc38
vovilonn <vovanegrshopa9782@gmail.com>
2021-12-04 19:16:08 +0200

    rebuild

--- public/component---src-pages-404-js-44b0ae1d916af9d99a29.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunknft=self.webpackChunknft||[]).push([[883],{59616:function(e,t,n){n.r(t);var l=n(67294),a=n(97219),r=n(93751);t.default=function(){return l.createElement(a.Z,null,l.createElement(r.Z,{refresh:!0,title:"FAPP"}),l.createElement("div",{className:"gen-wrap not-found-wrap"},l.createElement("h1",null,"404"),l.createElement("h2",null,"Page Not Found")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-44b0ae1d916af9d99a29.js.map
